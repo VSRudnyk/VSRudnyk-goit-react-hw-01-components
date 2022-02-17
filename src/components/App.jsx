7cd4d33558d0330b74eef30156f9bb6bbf6bb3f9
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-02-17 13:39:04 +0200

    add Profile and Ststistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
+import Profile from './Social-profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from '../data/user.json';
+import data from '../data/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import './Statistics.css';
+
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">{title}</h2>
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li className="item" key={stat.id}>
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default Statistics;
